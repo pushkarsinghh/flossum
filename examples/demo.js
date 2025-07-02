99b18bf962344e49d278b2bd4dd0921e120bf725
pushkarsinghh <pushkarkumarsingh1970@gmail.com>
2025-07-02 22:56:46 +0530

    Added wave Animation

---
+++ examples/demo.js
@@ -0,0 +1,6 @@
+import { wave } from '../index.js';
+
+(async () => {
+  await wave("Pushkar is coding 🛠️", { duration: 4000 });
+  await typeOut("This is a typing animation demo.", 100);
+})();
---
+++ index.js
@@ -0,0 +1,2 @@
+export { wave } from './lib/wave.js';
+export { typeOut } from './lib/type.js';
